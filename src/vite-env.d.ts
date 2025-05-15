5ad5be83884ef0317cc8d91c1d32719a5f3831e7
parksiwoon <parksw003@gmail.com>
2025-05-15 16:09:25 +0900

    ✨ feat: VITE_SPOTIFY_CLIENT_ID 및 VITE_SPOTIFY_CLIENT_SECRET 환경 변수 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,8 @@
 
 interface ImportMetaEnv {
   readonly VITE_OPENAI_APIKEY: string;
+  readonly VITE_SPOTIFY_CLIENT_ID: string;
+  readonly VITE_SPOTIFY_CLIENT_SECRET: string;
 }
 
 // eslint-disable-next-line no-unused-vars
