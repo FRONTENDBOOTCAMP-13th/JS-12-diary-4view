f974f07114132ea6471d0711f02a47292d86f957
parksiwoon <parksw003@gmail.com>
2025-05-19 15:46:35 +0900

    ✨ feat: 랜덤 문자열 생성 함수 추가

---
+++ src/utils/utils.ts
@@ -0,0 +1,12 @@
+// utils/utils.ts
+/**
+ * 지정된 길이의 랜덤 문자열 생성
+ */
+export function generateRandomString(length: number): string {
+  const possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const values = crypto.getRandomValues(new Uint8Array(length));
+  return Array.from(values)
+    .map(x => possible[x % possible.length])
+    .join('');
+}
