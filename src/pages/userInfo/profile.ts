052820a6d2fee2154f43e470dfe944afe6774737
sawajirikim <viperbmjs@naver.com>
2025-05-22 16:22:53 +0900

    ✨ feat: DOM로드시 로컬스토리지의 프로필 정보 인풋 필드에 자동 채우기

--- src/pages/userInfo/profile.ts
+++ src/pages/userInfo/profile.ts
@@ -1,18 +1,20 @@
 import '/src/style.css';
 
 document.addEventListener('DOMContentLoaded', () => {
+  // 뒤로가기 버튼
   const backButton = document.querySelector('#back') as HTMLButtonElement;
-
   backButton.addEventListener('click', () => {
     window.history.back();
   });
 
+  // form 요소에 제출 시 이벤트 리스너 추가
   const form = document.getElementById('profile-form') as HTMLFormElement;
 
   form.addEventListener('submit', event => {
     profileValidCheck(event, '/src/pages/userInfo/job.html');
   });
 
+  // 상단 job 페이지 링크에 이벤트 리스너 추가
   const jobPageLink = document.querySelector(
     '#to-job-page',
   ) as HTMLAnchorElement;
@@ -20,12 +22,27 @@ document.addEventListener('DOMContentLoaded', () => {
     profileValidCheck(event, '/src/pages/userInfo/job.html');
   });
 
+  // 상단 preference 페이지 링크에 이벤트 리스너 추가
   const preferencePageLink = document.querySelector(
     '#to-preference-page',
   ) as HTMLAnchorElement;
   preferencePageLink.addEventListener('click', event => {
     profileValidCheck(event, '/src/pages/userInfo/preference.html');
   });
+
+  // dom loaded 시 localStorage에 저장된 프로필 정보 불러오기
+  const nicknameInput = document.getElementById('nickname') as HTMLInputElement;
+  const ageInput = document.getElementById('age') as HTMLInputElement;
+  const profile = localStorage.getItem('profile');
+  if (profile) {
+    try {
+      const { nickname, age } = JSON.parse(profile);
+      nicknameInput.value = nickname;
+      ageInput.value = age;
+    } catch (err) {
+      console.warn('프로필 정보 불러오기 실패 :', err);
+    }
+  }
 });
 
 const profileValidCheck = function (event: Event, link: string) {
