92a3399d52284420b476e46d5390dc74370f0d3a
sawajirikim <viperbmjs@naver.com>
2025-05-22 16:18:59 +0900

    🐛 fix: profile.html에서 임의로 다른 단게로 넘어갈 수 있는 문제 해결

--- src/pages/userInfo/profile.ts
+++ src/pages/userInfo/profile.ts
@@ -10,64 +10,46 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('profile-form') as HTMLFormElement;
 
   form.addEventListener('submit', event => {
-    event.preventDefault();
+    profileValidCheck(event, '/src/pages/userInfo/job.html');
+  });
 
-    const nicknameInput = document.getElementById(
-      'nickname',
-    ) as HTMLInputElement;
-    const ageInput = document.getElementById('age') as HTMLInputElement;
+  const jobPageLink = document.querySelector(
+    '#to-job-page',
+  ) as HTMLAnchorElement;
+  jobPageLink.addEventListener('click', event => {
+    profileValidCheck(event, '/src/pages/userInfo/job.html');
+  });
 
-    const nickname = nicknameInput.value.trim();
-    const age = ageInput.value.trim();
-
-    if (!isNicknameValid(nickname)) {
-      alert('닉네임은 2~11자 내로 작성해주세요.');
-      nicknameInput.focus();
-      return;
-    }
-
-    const ageValidation = isAgeValid(age);
-
-    if (ageValidation.result === false) {
-      alert(ageValidation.message);
-      ageInput.focus();
-      return;
-    }
-
-    const profile = {
-      nickname,
-      age,
-    };
-
-    localStorage.setItem('profile', JSON.stringify(profile));
-
-    window.location.href = '/src/pages/userInfo/job.html';
+  const preferencePageLink = document.querySelector(
+    '#to-preference-page',
+  ) as HTMLAnchorElement;
+  preferencePageLink.addEventListener('click', event => {
+    profileValidCheck(event, '/src/pages/userInfo/preference.html');
   });
 });
 
-// 닉네임 유효성 검사
-function isNicknameValid(nickname: string): boolean {
-  console.log(nickname.length);
-  if (nickname.length < 2 || nickname.length > 11) {
-    return false;
+const profileValidCheck = function (event: Event, link: string) {
+  const form = document.getElementById('profile-form') as HTMLFormElement;
+
+  // 브라우저 기본 검증을 유도
+  if (!form.checkValidity()) {
+    form.reportValidity();
+    event.preventDefault();
+    return;
   }
 
-  return true;
-}
+  const nicknameInput = document.getElementById('nickname') as HTMLInputElement;
+  const ageInput = document.getElementById('age') as HTMLInputElement;
 
-// 나이 유효성 검사
-function isAgeValid(age: string): { result: boolean; message: string } {
-  // age에 문자가 포함되어 있는지 검사
-  for (const char of age) {
-    if (isNaN(Number(char))) {
-      return { result: false, message: '나이는 숫자만 입력해주세요.' };
-    }
-  }
+  const nickname = nicknameInput.value.trim();
+  const age = ageInput.value.trim();
 
-  // age의 범위 검사
-  if (Number(age) < 0 || Number(age) > 99) {
-    return { result: false, message: '나이는 0~99세 사이로 입력해주세요.' };
-  }
+  const profile = {
+    nickname,
+    age,
+  };
 
-  return { result: true, message: '' };
-}
+  localStorage.setItem('profile', JSON.stringify(profile));
+
+  window.location.href = link;
+};
